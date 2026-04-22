a1c0d57720630512aff644b18dec93b2f8bb466b
selma8080-ui <selma.kab8080@gmail.com>
2026-04-22 12:15:10 +0100

    ....

--- server/ram.php
+++ server/ram.php
@@ -12,7 +12,102 @@ $total = $memLine[1];
 $used = $memLine[2];
 $available = $memLine[6];
 
-echo "RAM Totale: " . $total . " Mo\n";
-echo "RAM Utilisée: " . $used . " Mo\n";
-echo "RAM Disponible: " . $available . " Mo\n";
+$percent = ($used / $total) * 100;
 ?>
+ 
+
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
+    <script src="https://cdn.jsdelivr.net/npm/echarts"></script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+
+    <div id="app">
+       <div id="chart" style="width:600px;height:400px;"></div>
+    </div>
+
+ <script>
+const app = Vue.createApp({
+    data(){
+        return{
+            ramPercent: <?= round($percent,2) ?>,
+            chart: null
+        }
+    },
+
+    methods:{
+        initChart() {
+            this.chart = echarts.init(document.getElementById('chart'));
+
+            let option = {
+                series: [
+                    {
+                        type: 'gauge',
+                        axisLine: {
+                            lineStyle: {
+                                width: 30,
+                                color: [
+                                    [0.3, '#67e0e3'],
+                                    [0.7, '#37a2da'],
+                                    [1, '#fd666d']
+                                ]
+                            }
+                        },
+                        pointer: {
+                            itemStyle: {
+                                color: 'auto'
+                            }
+                        },
+                        axisTick: {
+                            distance: -30,
+                            length: 8,
+                            lineStyle: {
+                                color: '#fff',
+                                width: 2
+                            }
+                        },
+                        splitLine: {
+                            distance: -30,
+                            length: 30,
+                            lineStyle: {
+                                color: '#fff',
+                                width: 4
+                            }
+                        },
+                        axisLabel: {
+                            color: 'inherit',
+                            distance: 40,
+                            fontSize: 20
+                        },
+                        detail: {
+                            formatter: '{value} %'
+                        },
+                        data: [
+                            {
+                                value: this.ramPercent
+                            }
+                        ]
+                    }
+                ]
+            };
+
+            this.chart.setOption(option);
+        }
+    },
+
+    mounted(){
+        this.initChart();
+    }
+});
+
+app.mount('#app');
+</script>
--- server/swap.php
+++ server/swap.php
@@ -7,19 +7,42 @@ Swap:           1024           0        1024
 
 
 
-
 $free_output = shell_exec('free -m');
 
+preg_match('/Swap:\s+(\d+)\s+(\d+)\s+(\d+)/', $free_output, $matches);
 
-if (preg_match('/Swap:\s+(\d+)\s+(\d+)\s+(\d+)/', $free_output, $matches)) {
-    $totalSwap = $matches[1];
-    $usedSwap  = $matches[2]; 
-    $freeSwap  = $matches[3]; 
+$totalSwap = $matches[1];
+$usedSwap  = $matches[2];
 
-    echo "Total Swap: " . $totalSwap . " Mo\n";
-    echo "Utilisé Swap: " . $usedSwap . " Mo\n";
-    echo "Disponible Swap: " . $freeSwap . " Mo\n";
-} else {
-    echo "Impossible de récupérer les informations de Swap.";
-}
+echo json_encode([
+  "used" => (int)$usedSwap,
+  "time" => time()
+]);
 ?>
+
+
+
+<!DOCTYPE html>
+ <html lang="en">
+ <head>
+
+    <script src="https://cdn.jsdelivr.net/npm/vue@3"></script>
+    <script src="https://cdn.jsdelivr.net/npm/echarts"></script>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+ </head>
+ <body>
+
+    <div id="app">
+       <div id="chart" style="width:600px;height:400px;"></div>
+    </div>
+
+ <script>
+let myChart = echarts.init(document.getElementById('chart'));
+
+let data = [];
+
+
+
