5d022ccaec3f13fe1c6742c3697e381e8055fadb
HibaAd <hiba159hiba@gmail.com>
2026-04-23 13:55:32 +0100

    dn

--- server/trafficDisque.php
+++ server/trafficDisque.php
@@ -1,5 +1,14 @@
+<?php
 
+$data = [
+    [strtotime("2026-04-23 08:00") * 1000, 120000000], 
+    [strtotime("2026-04-23 09:00") * 1000, 180000000], 
+    [strtotime("2026-04-23 10:00") * 1000, 220000000],
+    [strtotime("2026-04-23 11:00") * 1000, 150000000],
+    [strtotime("2026-04-23 12:00") * 1000, 300000000], 
+];
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,156 +26,105 @@
     </div>
 
     <script>
-    const app = Vue.createApp({
-      data() {
-        return {
-          chart: null
-        }
-      },
-      mounted() {
-        this.initChart();
-      },
-      methods: {
-        initChart() {
-          this.chart = echarts.init(document.getElementById('chart'));
-
-          const TOTAL_POINTS = 100;
-          const SIX_HOURS_MS = 6 * 3600 * 1000;
-          const INTERVAL = SIX_HOURS_MS / TOTAL_POINTS; 
-          
-          let base = +new Date(2026, 3, 23, 8, 0, 0); 
-          let data = [];
-
-          for (let i = 0; i < TOTAL_POINTS; i++) {
-            let now = new Date(base + i * INTERVAL);
-            data.push([+now, Math.round(Math.random() * 5000)]); 
-          }
-
-          const option = {
-            tooltip: {
-              trigger: 'axis',
-              formatter: (params) => {
-                let date = new Date(params[0].value[0]);
-                let timeStr = date.getHours() + ':' + date.getMinutes().toString().padStart(2, '0');
-                return timeStr + '<br/>Trafic : ' + params[0].value[1].toLocaleString() + ' Octets';
+      const app = Vue.createApp({
+          data() {
+              return {
+                  chart: null,
+                  data: <?= json_encode($data ?? []) ?>
               }
-            },
-            xAxis: {
-              type: 'time',
-              boundaryGap: false,
-              axisLabel: { formatter: '{HH}:{mm}', hideOverlap: true }
-            },
-            yAxis: {
-              type: 'value',
-              name: 'Débit',
-              axisLabel: {
-                formatter: function (value) {
-                  if (value >= 1024 * 1024) return (value / (1024 * 1024)).toFixed(1) + ' Mo';
-                  if (value >= 1024) return (value / 1024).toFixed(1) + ' Ko';
-                  return value + ' B';
-                }
-              }
-            },
-            series: [{
-              name: 'Trafic Octets',
-              type: 'line',
-              smooth: true,
-              symbol: 'none',
-              areaStyle: {
-                color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-                  { offset: 0, color: 'rgb(58, 77, 233)' },
-                  { offset: 1, color: 'rgb(15, 20, 50)' }
-                ])
-              },
-              data: data
-            }]
-          };
-
-          this.chart.setOption(option);
-
-          setInterval(() => {
-            let lastTime = data[data.length - 1][0];
-            let nextTime = lastTime + INTERVAL;
-            let nextValue = Math.round(Math.random() * 5000);
-
-            data.shift(); 
-            data.push([nextTime, nextValue]);
-
-            this.chart.setOption({
-              series: [{ data: data }]
-            });
-          }, 2000); 
-        }
-      }
-    }).mount('#app');
-<<<<<<< HEAD
-=======
-=======
-
-      const option = {
-        tooltip: {
-          trigger: 'axis',
-          formatter: (params) => {
-            let date = new Date(params[0].value[0]);
-            let timeStr = date.getHours() + ':' + date.getMinutes().toString().padStart(2, '0');
-            return timeStr + '<br/>Trafic : ' + params[0].value[1].toLocaleString() + ' Octets';
-          }
-        },
-       xAxis: {
-  type: 'time',
-  boundaryGap: false,
-  axisLabel: {
-    formatter: function (value) {
-      let date = new Date(value);
-      return date.getHours() + ':' + String(date.getMinutes()).padStart(2, '0');
-    }
-  }
-},
-yAxis: {
-          type: 'value',
-          name: 'Débit',
-          axisLabel: {
-            formatter: function (value) {
-              if (value >= 1024 * 1024) return (value / (1024 * 1024)).toFixed(1) + ' Mo';
-              if (value >= 1024) return (value / 1024).toFixed(1) + ' Ko';
-              return value + ' B';
-            }
-          }
-        },
-        series: [{
-          name: 'Trafic Octets',
-          type: 'line',
-          smooth: true,
-          symbol: 'none',
-          areaStyle: {
-            color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              { offset: 0, color: 'rgb(58, 77, 233)' },
-              { offset: 1, color: 'rgb(15, 20, 50)' }
-            ])
           },
-          data: data
-        }]
-      };
 
-      this.chart.setOption(option);
+          methods: {
+              formatHour(ts) {
+                  let d = new Date(ts);
+                  return d.getHours().toString().padStart(2, '0') + ":00";
+              },
 
-      setInterval(() => {
-        let lastTime = data[data.length - 1][0];
-        let nextTime = lastTime + INTERVAL;
-        let nextValue = <?= $traffic ?>;
+              formatBytes(bytes) {
+                  if (bytes < 1024) return bytes + " B";
+                  if (bytes < 1024 ** 2) return (bytes / 1024).toFixed(1) + " KB";
+                  if (bytes < 1024 ** 3) return (bytes / 1024 ** 2).toFixed(1) + " MB";
+                  return (bytes / 1024 ** 3).toFixed(1) + " GB";
+              },
 
-        data.shift(); 
-        data.push([nextTime, nextValue]);
+              filterLast6Hours() {
+                  let sixHoursAgo = Date.now() - (6 * 3600 * 1000);
 
-        this.chart.setOption({
-          series: [{ data: data }]
-        });
-      }, 2000); 
-    }
-  }
-}).mount('#app');
->>>>>>> bb30613c2444bcc8804b85574c6f689cece5de08
->>>>>>> 4ebfc0dc513c76d01760168d6cfe91e3e22243de
-</script>
+                  this.data = this.data.filter(item => item[0] >= sixHoursAgo);
+              },
+
+              initChart() {
+                this.chart = echarts.init(document.getElementById("chart"));
+
+                this.chart.setOption({
+                    title: {
+                        left: 'center',
+                        text: 'Traffic Disque'
+                    },
+
+                    tooltip: {
+                        trigger: 'axis',
+                        position: function (pt) {
+                            return [pt[0], '10%'];
+                        },
+                        formatter: (params) => {
+                            let time = new Date(params[0].value[0]).toLocaleTimeString();
+                            let val = params[0].value[1];
+                            return `${time}<br>${this.formatBytes(val)}`;
+                        }
+                    },
+
+                    xAxis: {
+                        type: 'time',
+                        boundaryGap: false,
+                        axisLabel: {
+                            formatter: (value) => this.formatHour(value)
+                        }
+                    },
+
+                    yAxis: {
+                        type: 'value',
+                        boundaryGap: [0, '100%'],
+                        axisLabel: {
+                            formatter: (value) => this.formatBytes(value)
+                        }
+                    },
+
+                    dataZoom: [
+                        {
+                            type: 'inside',
+                            start: 0,
+                            end: 100
+                        },
+                        {
+                            start: 0,
+                            end: 100
+                        }
+                    ],
+
+                    series: [
+                        {
+                            name: 'Usage',
+                            type: 'line',
+                            smooth: true,
+                            symbol: 'none',
+                            areaStyle: {}, 
+                            data: this.data
+                        }
+                    ]
+                });
+            }
+          },
+
+          mounted() {
+              this.filterLast6Hours();
+
+              this.initChart();
+          }
+      });
+
+      app.mount('#app');
+    </script>
+
 </body>
 </html>
