e56dc541e597c294e10d967f44a285b270c2d962
selma8080-ui <selma.kab8080@gmail.com>
2026-04-30 13:12:17 +0100

    ..

--- server/trafficDisque.php
+++ server/trafficDisque.php
@@ -4,86 +4,93 @@
 
 <script>
 function BuildRamChart(data) {
+    var chartDom = document.getElementById('trafficDisqueChart');
+    var myChart = echarts.init(chartDom);
 
-    var trafficDisqueChart = document.getElementById('trafficDisqueChart');
-    var myChart = echarts.init(trafficDisqueChart);
-    
     let dataD = data;
-    let maxDuration = 6 * 3600 * 1000; 
+    let maxDuration = 6 * 3600 * 1000;
+
+    function getNow() {
+        return new Date().getTime();
+    }
 
     var option = {
-        tooltip: {
-            trigger: 'axis'
-        },
-        title: {
-            left: 'center',
-            text: 'Traffic Disque'
-        },
+        tooltip: { trigger: 'axis' },
+        title: { left: 'center', text: 'Traffic Disque' },
+
         xAxis: {
             type: 'time',
+            min: getNow() - maxDuration,
+            max: getNow(),
             axisLabel: {
                 formatter: function (value) {
                     let d = new Date(value);
                     return d.getHours() + ':' + String(d.getMinutes()).padStart(2, '0');
-                }
-            }
+                },
+                hideOverlap: true
+            },
+            splitNumber: 6,
+            splitLine: { show: false }
         },
+
         yAxis: {
             type: 'value',
             axisLabel: {
-                formatter: function (value) {
-                    return (value / (1024 * 1024)).toFixed(2) + ' MB';
-                }
+                formatter: v => (v / (1024 * 1024)).toFixed(2) + ' MB'
             }
         },
+
         series: [{
             name: 'Utilisation Disque',
             type: 'line',
             smooth: true,
             symbol: 'none',
-            areaStyle: {},
-            data: dataD   // ✔ CORRIGÉ
+            lineStyle: { color: '#5470c6', width: 2 },
+            areaStyle: { color: 'rgba(84, 112, 198, 0.3)' },
+            data: dataD
         }]
     };
 
     myChart.setOption(option);
 
     function addDataPoint(value) {
-        let now = new Date().getTime();
+        let now = getNow();
 
         dataD.push([now, value]);
 
-        // garder seulement les données sur 6h
-        dataD = dataD.filter(point => now - point[0] <= maxDuration);
+        // garde 6h
+        dataD = dataD.filter(p => now - p[0] <= maxDuration);
 
         myChart.setOption({
-            series: [{
-                data: dataD   // ✔ CORRIGÉ
-            }]
+            xAxis: {
+                min: now - maxDuration,
+                max: now
+            },
+            series: [{ data: dataD }]
         });
     }
 
-    // données initiales
-    let now = new Date().getTime();
-    for (let i = 0; i < 50; i++) {
-        let time = now - (50 - i) * 2000;
-        let value = Math.random() * 50 * 1024 * 1024;
-        dataD.push([time, value]);
+    // init fake data (6h réaliste)
+    let now = getNow();
+    for (let i = 0; i < 100; i++) {
+        dataD.push([
+            now - (6 * 3600 * 1000) + i * 360000, // espacé sur 6h
+            Math.random() * 50 * 1024 * 1024
+        ]);
     }
 
     myChart.setOption({
-        series: [{
-            data: dataD   // ✔ CORRIGÉ
-        }]
+        series: [{ data: dataD }],
+        xAxis: {
+            min: now - maxDuration,
+            max: now
+        }
     });
 
-    // mise à jour toutes les 2 secondes
     setInterval(() => {
-        let value = Math.random() * 50 * 1024 * 1024;
-        addDataPoint(value);
+        addDataPoint(Math.random() * 50 * 1024 * 1024);
     }, 2000);
 }
 
-// ✔ IMPORTANT : appel de la fonction
 BuildRamChart([]);
 </script>
--- server/trafficReseau.php
+++ server/trafficReseau.php
@@ -4,29 +4,49 @@
 
 <script>
 function BuildReseauChart(data) {
-
-    var trafficReseauChart = document.getElementById('trafficReseauChart');
-    var myChartR = echarts.init(trafficReseauChart);
+    var chartDom = document.getElementById('trafficReseauChart');
+    var myChartR = echarts.init(chartDom);
 
     let dataR = data;
     let maxDurationR = 6 * 3600 * 1000;
 
+    function getNow() {
+        return new Date().getTime();
+    }
+
     var optionR = {
         tooltip: { trigger: 'axis' },
         title: { left: 'center', text: 'Traffic Réseau' },
-        xAxis: { type: 'time' },
+
+        xAxis: {
+            type: 'time',
+            min: getNow() - maxDurationR,
+            max: getNow(),
+            axisLabel: {
+                formatter: function (value) {
+                    let d = new Date(value);
+                    return d.getHours() + ':' + String(d.getMinutes()).padStart(2, '0');
+                },
+                hideOverlap: true
+            },
+            splitNumber: 6,
+            splitLine: { show: false }
+        },
+
         yAxis: {
             type: 'value',
             axisLabel: {
                 formatter: v => (v / (1024 * 1024)).toFixed(2) + ' MB'
             }
         },
+
         series: [{
             name: 'Réseau',
             type: 'line',
             smooth: true,
             symbol: 'none',
-            areaStyle: {},
+            lineStyle: { color: '#5470c6', width: 2 },
+            areaStyle: { color: 'rgba(84, 112, 198, 0.3)' },
             data: dataR
         }]
     };
@@ -34,40 +54,42 @@ function BuildReseauChart(data) {
     myChartR.setOption(optionR);
 
     function addDataPointR(value) {
-        let nowR = new Date().getTime();
+        let now = getNow();
 
-        dataR.push([nowR, value]);
+        dataR.push([now, value]);
 
-        // garder seulement 6h
-        dataR = dataR.filter(point => nowR - point[0] <= maxDurationR);
+        dataR = dataR.filter(p => now - p[0] <= maxDurationR);
 
         myChartR.setOption({
-            series: [{
-                data: dataR
-            }]
+            xAxis: {
+                min: now - maxDurationR,
+                max: now
+            },
+            series: [{ data: dataR }]
         });
     }
 
-    // données initiales
-    let nowR = new Date().getTime();
-
-    for (let i = 0; i < 50; i++) {
-        let timeR = nowR - (50 - i) * 2000;
-        let valueR = Math.random() * 50 * 1024 * 1024;
-        dataR.push([timeR, valueR]);
+    // init 6h propre
+    let now = getNow();
+    for (let i = 0; i < 100; i++) {
+        dataR.push([
+            now - (6 * 3600 * 1000) + i * 360000,
+            Math.random() * 50 * 1024 * 1024
+        ]);
     }
 
     myChartR.setOption({
-        series: [{ data: dataR }]
+        series: [{ data: dataR }],
+        xAxis: {
+            min: now - maxDurationR,
+            max: now
+        }
     });
 
-    // 🔥 UPDATE TEMPS RÉEL (manquait chez toi)
     setInterval(() => {
-        let valueR = Math.random() * 50 * 1024 * 1024;
-        addDataPointR(valueR);
+        addDataPointR(Math.random() * 50 * 1024 * 1024);
     }, 2000);
 }
 
-// 🔥 APPEL (manquait chez toi)
 BuildReseauChart([]);
 </script>
